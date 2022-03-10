aa9a9761f73a8e53c55e655a105f1b72e4fa5b8b
mofosquito <esjole@protonmail.com>
2022-03-10 09:28:09 +0800

    Add project files.

---
+++ hopeuperman_wt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hopeuperman_wt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
